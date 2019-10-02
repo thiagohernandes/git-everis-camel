95f06f6af26f1f784701b2fcde7ef8862756edf6
Thiago Hernandes Souza <thiago.hernandes.souza@everis.com>
2019-10-02 11:52:44 -0300

    Módulo 1

--- projeto-base/src/main/java/com/camel/code/parte01/util/UtilApp.java
+++ projeto-base/src/main/java/com/camel/code/parte01/util/UtilApp.java
@@ -1,12 +1,9 @@
 package com.camel.code.parte01.util;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.RoutesBuilder;
-import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +15,6 @@ public class UtilApp {
 
 	 private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	 
-	 private final String rotaKey = "rota";
-	
 	 public void runContextCamel(List<Object> listRoutes, int timeMilisegundos, boolean alwaysRunning) throws Exception {
 		 	logger.info("*********** Iniciando o contexto do Camel");
 	        CamelContext ctx = new DefaultCamelContext();
